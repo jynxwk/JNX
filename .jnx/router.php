5052ee5547b31e3bff223824d331536fdef65d01
Jynx <thekillercraftlp@gmail.com>
2023-08-20 15:33:57 +0200

    alpha - v0.3 - Fixed routing: Depending on the subdirectories you installed it in, it wouldn't have worked, fixed that.

--- .jnx/router.php
+++ .jnx/router.php
@@ -2,76 +2,81 @@
 include('functions.php');
 $input = file_get_contents('php://input');
 $data = json_decode($input, true);
-$path = $data['path'];
 
-$app = "../src/app.html";
-$dir = "../src/pages/" . $path . "/";
-$page = $dir . getConfig("standard-file");
+if (isset($data['path'])) {
+    echo loadPage($data['path']);
+}
 
-// Check if app.html exists
-if (file_exists($app)) {
-    // Check if directory exists
-    if (file_exists($page)) {
+function loadPage($path = "", $self = ".jnx/router.php") {
+    $app = "../src/app.html";
+    $base = str_replace($self, "", $_SERVER['PHP_SELF']);
+    $path = str_replace($base, "", $path);
+    $dir = "../src/pages/" . $path . "/";
+    $page = $dir . getConfig("standard-file");
+
+    // Check if app.html exists
+    if (file_exists($app)) {
         $app = file_get_contents($app);
-        $page = file_get_contents($page);
-        if ($page) {
-            $head = "";
-            $body = "";
-            $style = "";
-            $script = "";
-
-            // Page Head
-            if (str_contains($app, "<head>")) {
-                $head = $head . getContentBetween($app, "<head>", "</head>");
+        // Check if directory exists
+        if (file_exists($page)) {
+            $page = file_get_contents($page);
+            if ($page) {
+                $head = "";
+                $body = "";
+                $style = "";
+                $script = "";
+    
+                // Page Head
+                if (str_contains($page, "<head>")) {
+                    $head = $head . getContentBetween($page, "<head>", "</head>");
+                }
+                
+                // Style
+                if (str_contains($page, "<style>")) {
+                    $style = getContentBetween($page, "<style>", "</style>");
+                    $style = "<style>".$style."</style>";
+                    $head = $head . $style;
+                }
+    
+                $style = $dir . getConfig("style-file");
+                if (file_exists($style)) {
+                    $style = file_get_contents($style);
+                    $style = "<style>" . $style . "</style>";
+                    $head = $head . $style;
+                }
+    
+                // Script
+                if (str_contains($page, "<script>")) {
+                    $script = getContentBetween($page, "<script>", "</script>");
+                    $script = "<script>" . $script . "</script>";
+                    $head = $head . $script;
+                }
+    
+                $script = $dir . getConfig("script-file");
+                if (file_exists($script)) {
+                    $script = file_get_contents($script);
+                    $script = "<script>" . $script . "</script>";
+                    $head = $head . $script;
+                }
+                
+                // Remove Tags from body
+                $body = $page;
+                $body = preg_replace("~<head>[^.]*</head>~", "", $body);
+                $body = preg_replace("~<style>[^.]*</style>~", "", $body);
+                $body = preg_replace("~<script>[^.]*</script>~", "", $body);
+                
+                // Return head & body as json
+                $response = array("app" => $app, "head" => $head, "body" => $body);
+                return json_encode($response);
+            } else {
+                echo "Error reading page.";
             }
-            if (str_contains($page, "<head>")) {
-                $head = $head . getContentBetween($page, "<head>", "</head>");
-            }
-            
-            // Style
-            if (str_contains($page, "<style>")) {
-                $style = getContentBetween($page, "<style>", "</style>");
-                $style = "<style>".$style."</style>";
-                $head = $head . $style;
-            }
-
-            $style = $dir . getConfig("style-file");
-            if (file_exists($style)) {
-                $style = file_get_contents($style);
-                $style = "<style>" . $style . "</style>";
-                $head = $head . $style;
-            }
-
-            // Script
-            if (str_contains($page, "<script>")) {
-                $script = getContentBetween($page, "<script>", "</script>");
-                $script = "<script>" . $script . "</script>";
-                $head = $head . $script;
-            }
-
-            $script = $dir . getConfig("script-file");
-            if (file_exists($script)) {
-                $script = file_get_contents($script);
-                $script = "<script>" . $script . "</script>";
-                $head = $head . $script;
-            }
-            
-            // Remove Tags from body
-            $body = $page;
-            $body = preg_replace("~<head>[^.]*</head>~", "", $body);
-            $body = preg_replace("~<style>[^.]*</style>~", "", $body);
-            $body = preg_replace("~<script>[^.]*</script>~", "", $body);
-            
-            // Return head & body as json
-            $response = array("head" => $head, "body" => $body);
-            echo json_encode($response);
         } else {
-            echo "Error reading page.";
+            echo "Page not found.";
         }
     } else {
-        echo "Page not found.";
+        echo "app.html is missing.";
     }
-} else {
-    echo "app.html is missing.";
 }
+
 ?>
